98b2aa745841ceb7d67ef67f597315cf62af813c
alceoone <fajar.dm2k@gmail.com>
2024-01-28 19:56:52 +0700

    update link image

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,9 +68,10 @@ return [
     | the locations of the links and the values should be their targets.
     |
     */
-
+    
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('images') => storage_path('app/images'),
     ],
 
 ];
